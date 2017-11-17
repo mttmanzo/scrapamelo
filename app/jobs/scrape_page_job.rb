faa84f15a54e27079aab223d4a2c8715fa9ab995
Silvio Relli <silvio@relli.it>
2017-11-18 00:28:24 +0100

    Twilio calls working

--- app/jobs/scrape_page_job.rb
+++ app/jobs/scrape_page_job.rb
@@ -12,8 +12,8 @@ class ScrapePageJob < ApplicationJob
     data = Nokogiri::HTML(browser.html)
     prices = data.css('.itemPrice').text
     prices.gsub('€', '').split(' ').each do |p|
-      puts p
-      # if p == '0,99'
+      # puts p
+      # if p == '37,99'
       if p == '0,99'
         logger.info "C'è roba bona!"
         price_found = true
@@ -38,7 +38,26 @@ class ScrapePageJob < ApplicationJob
 
     @client = Twilio::REST::Client.new account_sid, auth_token
 
+    # silvio
+    @client.api.account.calls.create(
+      from: ENV['PHONE_CALLER'],
+      to: ENV['PHONE1'],
+      url: 'https://demo.twilio.com/welcome/voice/'
+    )
 
-    # todo
+    # gdmn
+    # @client.api.account.calls.create(
+    #   from: ENV['PHONE_CALLER'],
+    #   to: ENV['PHONE2'],
+    #   url: 'https://demo.twilio.com/welcome/voice/'
+    # )
+
+    # matte
+    # @client.api.account.calls.create(
+    #   from: ENV['PHONE_CALLER'],
+    #   to: ENV['PHONE3'],
+    #   url: 'https://demo.twilio.com/welcome/voice/'
+    # )
+
   end
 end
